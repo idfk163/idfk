9c4723120c925cda0bb1959b6fcc40305f2705a8
idfk163 <53252907+idfk163@users.noreply.github.com>
2019-07-24 09:51:17 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("603493500235022336")
+setInterval(function() {
+channel.send(`0hCool:joy:`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
